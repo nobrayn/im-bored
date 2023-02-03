a40d9d4e5884cde417b9c0d977632792b6b2d8c7
nobrayn <nobrayn@gmail.com>
2023-02-03 18:20:14 -0500

    user choices getting sent to API, response rendered to page

--- src/App.js
+++ src/App.js
@@ -1,88 +1,74 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+// components
+import UserOptions from './Components/UserOptions';
+
 import './App.css'
 
 function App() {
+  const [response, setResponse] = useState('');
 
-  // Querying boredAPI
-    // Currently is returning random "receational" type
-  // useEffect(() => {
-  //   // query boredAPI
-  //   const fetchData = async () => {
-  //     // construct base URL
-  //     const url = new URL('http://www.boredapi.com/api/activity?');
-  //     // adding chosen parameters
-  //     url.search = new URLSearchParams({
-  //       type: 'recreational',
-  //     });
+  // props for UserOptions
+  const getData = async (activity, price, people) => {
+    let minPrice = 0
+    let maxPrice = 0
+    let minParticipants = 1
+    let maxParticipants = 1
 
-  //     // try/catch for error handling
-  //     try {
-  //       const data = await axios.get(url);
-  //       // console.log(data.data.activity)
-  //       const response = (data.data.activity);
-  //       console.log(response)
-  //     } catch (error) {
-  //       // err handle
-  //       console.log('error, dingus')
-  //     }
-  //   }
-  //   fetchData();
+    // price
+    if (price === 'cheap') {
+      minPrice = 0.1
+      maxPrice = 0.4
+    }
+    if (price === 'notCheap') {
+      minPrice = 0.5
+      maxPrice = 1
+    }
 
-  // }, [])
+    // participants
+    if (people === 'duo') {
+      minParticipants = 2
+      maxParticipants = 2
+    }
+    if (people === 'group') {
+      minParticipants = 3
+      maxParticipants = 6
+    }
+
+
+    // construct base URL
+    const url = new URL('http://www.boredapi.com/api/activity?');
+    // adding chosen parameters
+    url.search = new URLSearchParams({
+      type: activity,
+      minprice: minPrice,
+      maxprice: maxPrice,
+      minparticipants: minParticipants,
+      maxparticipants: maxParticipants
+    });
+    // console.log(url)
+    // try/catch for error handling
+    try {
+      const data = await axios.get(url);
+      setResponse(data.data.activity);
+    } catch (error) {
+      // err handle
+      console.log('error, dingus')
+    }
+  }
 
   return (
-    <div className="App">
-      <h1>Need something to do?</h1>
-      <h2>Narrow it down, or go with "Any"!</h2>
-        <form action="" className="userSelects">
-          <div className="formContainer">
-            <div className="formLeft">
-              <label htmlFor="activityType">What type of activity?</label>
-              <label htmlFor="priceRange">What's your price range?</label>
-              <label htmlFor="people">How many people?</label>
-            </div>
-
-            <div className="formRight">
-              <div className="block">
-                <select name="activityType" id="activityType">
-                  <option value="initial" selected>Any</option>
-                  <option value="education">Education</option>
-                  <option value="recreational">Recreational</option>
-                  <option value="social">Social</option>
-                  <option value="diy">DIY</option>
-                  <option value="charity">Charity</option>
-                  <option value="cooking">Cooking</option>
-                  <option value="music">Music</option>
-                  <option value="busywork">Busywork</option>
-                </select>
-              </div>
-              
-              <div className="block">
-                <select name="priceRange" id="priceRange">
-                  <option value="initial" selected>Any</option>
-                  <option value="free">Free</option>
-                  <option value="cheap">Cheap</option>
-                  <option value="notCheap">Not so cheap</option>
-                </select>
-              </div>
-
-              <div className="block">
-                <select name="people" id="people">
-                  <option value="initial" selected>Any</option>
-                  <option value="solo">Just you</option>
-                  <option value="duo">You and a friend</option>
-                  <option value="group">You and some friends</option>
-                </select>
-              </div>
-            </div> {/* end of formRight */}
-          </div> {/* end of formContainer */}
-
-          <button className="submit">Submit</button>
-        </form>
+    <div>
+      {/* passing props to UserOptions */}
+      <UserOptions getData={getData}/>
+      <div>
+        <h2>{response}</h2>
+        <button onClick={'fart'}>Try again?</button>
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/UserOptions.js
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+
+const UserOptions = ({getData}) => {
+
+  const [userTypeChoice, setUserTypeChoice] = useState('');
+  const [userPriceChoice, setUserPriceChoice] = useState('');
+  const [userNumberChoice, setUserNumberChoice] = useState('');
+  const [showForm, setShowForm] = useState(true);
+
+
+  // stretch goal: combine these into one function
+  const handleUserTypeChoice = (e) => {
+    setUserTypeChoice(e.target.value)
+  }
+
+  const handleUserPriceChoice = (e) => {
+    setUserPriceChoice(e.target.value)
+  }
+
+  const handleUserNumberChoice = (e) => {
+    setUserNumberChoice(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // receiving props from App.js
+    getData(userTypeChoice, userPriceChoice, userNumberChoice)
+    setShowForm(false);
+  }
+
+  console.log(setUserTypeChoice)
+  return (
+    showForm && (
+    <section>
+      <div className="App">
+        <h1>Need something to do?</h1>
+        <h2>Narrow it down, or go with "Any"!</h2>
+        <form onSubmit={handleSubmit} action="" className="userSelects">
+          <div className="formContainer">
+            <div className="formLeft">
+
+              {/* next to selects, flex 50% */}
+              <label htmlFor="activityType">What type of activity?</label>
+              <label htmlFor="priceRange">What's your price range?</label>
+              <label htmlFor="people">How many people?</label>
+            </div>
+
+            <div className="formRight">
+              <div className="block">
+                <select value={userTypeChoice} onChange={handleUserTypeChoice} id="activityType" name="activityType">
+                  <option value="initial" defaultValue="initial">Any</option>
+                  <option value="education">Education</option>
+                  <option value="recreational">Recreational</option>
+                  <option value="social">Social</option>
+                  <option value="cooking">Cooking</option>
+                  <option value="music">Music</option>
+                  {/* the following four options, when selected, must disable the people select/set it to 1 */}
+                  <option value="relaxation">Relaxation</option>
+                  <option value="diy">DIY</option>
+                  <option value="charity">Charity</option>
+                  <option value="busywork">Busywork</option>
+                </select>
+              </div>
+
+              <div className="block">
+                <select value={userPriceChoice} onChange={handleUserPriceChoice} name="priceRange" id="priceRange">
+                  <option value="initial" defaultValue="initial">Any</option>
+                  {/* free: price=0 */}
+                  <option value="free">Free</option>
+                  {/* cheap: minprice=0.1&maxprice=0.4 */}
+                  <option value="cheap">Cheap</option>
+                  {/* not so cheap: minprice=0.5&maxprice=1 */}
+                  <option value="notCheap">Not so cheap</option>
+                </select>
+              </div>
+
+              <div className="block">
+                <select value={userNumberChoice} onChange={handleUserNumberChoice} name="people" id="people">
+                  <option value="initial" defaultValue="initial">Any</option>
+                  {/* solo: participants=1 */}
+                  <option value="solo">Just you</option>
+                  {/* duo: participants=2 */}
+                  <option value="duo">You and a friend</option>
+                  {/* group: minparticipants=3&maxparticipants=6 */}
+                  <option value="group">You and some friends</option>
+                </select>
+              </div>
+            </div> {/* end of formRight */}
+          </div> {/* end of formContainer */}
+
+          <button className="submit">Submit</button>
+        </form>
+      </div>
+    </section>
+    )
+  )
+}
+
+export default UserOptions;
