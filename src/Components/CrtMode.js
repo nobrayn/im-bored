965fcd9cc9f4c64d9124bec80d205f9cf4e90b41
nobrayn <nobrayn@gmail.com>
2023-02-08 09:54:02 -0500

    tidied up some code, adjusted media queries

--- src/Components/CrtMode.js
+++
@@ -1,8 +0,0 @@
-const CrtMode = ({ showStyling, setShowStyling }) => (
-  <button onClick={(e) => {
-    e.preventDefault();
-    setShowStyling(!showStyling);
-  }}>Toggle Style</button>
-);
-
-export default CrtMode;
